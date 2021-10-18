4dc60c27ce61963c7b08d910c777f5d3c0b6694a
Yanis Dib <ynsprive@gmail.com>
2021-10-18 19:08:46 +0200

    Create MenuButton.jsx

---
+++ src/components/MenuButton/MenuButton.jsx
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+import UserPicture from './UserPicture';
+import { Menu as Icon } from '@styled-icons/feather/Menu';
+
+
+const Button = styled.div`
+    width: 70px;    
+    height: 35px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    justify-self: end;
+    text-align: center;
+    background-color: #3e3e3e;
+    color: white;
+    padding: 7px 12px;
+    border-radius: 1.5rem;
+    float: right;
+    cursor: pointer;
+`;
+
+const MenuIcon = styled(Icon)`
+    height: 20px;
+    color: white;
+    stroke-width: 1.5px;
+`;
+
+function MenuButton({ userPicture, onClick }) {
+    return (
+        <Button onClick={onClick}>
+            <MenuIcon />
+            <UserPicture picture={userPicture} />
+        </Button>
+    );
+}
+
+MenuButton.defaultProps = {
+    onClick: () => { },
+};
+
+MenuButton.propTypes = {
+    onClick: PropTypes.func,
+};
+
+export default MenuButton;
