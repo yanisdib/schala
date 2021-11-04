85dfcf9b940ae4a31ce1bb12b3b108243c6e9b93
Yanis Dib <ynsprive@gmail.com>
2021-11-04 20:16:23 +0100

    Create component exports

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+// COMPONENTS EXPORTS
+
+export { default as Button } from './Button/Button';
+export { default as MenuButton } from './MenuButton/MenuButton';
+export { default as Header } from './Header/Header';
