cb87a29590e201a47a213efcecfe79a98b83a09b
Yanis Dib <ynsprive@gmail.com>
2021-11-04 20:15:13 +0100

    Update AppRouter.jsx

--- client/src/routers/AppRouter.jsx
+++ client/src/routers/AppRouter.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Switch, Route } from 'react-router';
 import App from '../App';
+import { Header } from '../components';
 
 function AppRouter() {
     return (
         <Router>
+            <Header />
             <main>
                 <Switch>
                     <Route component={App} path="/" exact />
