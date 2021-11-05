10b09dc19771cfaa5b571d7557b57edde637760f
Yanis Dib <ynsprive@gmail.com>
2021-11-05 15:46:41 +0100

    Add export for Tag component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,7 @@
 // COMPONENTS EXPORTS
 
+export { default as Header } from './Header/Header';
+
 export { default as Button } from './Button/Button';
 export { default as MenuButton } from './MenuButton/MenuButton';
-export { default as Header } from './Header/Header';
+export { default as Tag } from './Tag/Tag';
