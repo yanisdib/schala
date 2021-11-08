89937a14a14e964331dace039b5cf31a02fcec3f
Yanis Dib <ynsprive@gmail.com>
2021-11-08 20:46:04 +0100

    Add new components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,4 +4,10 @@ export { default as Header } from './Header/Header';
 
 export { default as Button } from './Button/Button';
 export { default as MenuButton } from './MenuButton/MenuButton';
+export { default as DisplayIdentifiedButton } from './DisplayIdentifiedButton/DisplayIdentifiedButton';
+
 export { default as Tag } from './Tag/Tag';
+export { default as Pointer } from './Pointer/Pointer';
+export { default as Bubble } from './Bubble/Bubble';
+
+export { default as PictureIdentifier } from './PictureIdentifier/PictureIdentifier';
