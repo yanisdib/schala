9b5258e42841d711fcf7deacb66b72a8f1b7e277
Yanis Dib <ynsprive@gmail.com>
2021-11-08 20:43:17 +0100

    Update UserPicture component tests

---
+++ client/src/tests/components/MenuButton/UserPicture.test.jsx
@@ -0,0 +1,17 @@
+import { mount } from 'enzyme';
+import UserPicture from '../../../components/MenuButton/UserPicture';
+
+describe('UserPicture component', () => {
+    let wrapper;
+    const defaultProps = UserPicture.defaultProps.picture;
+
+    beforeEach(() => wrapper = mount(<UserPicture />));
+
+    it('renders properly', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    it('renders with default picture props', () => {
+        expect(defaultProps).toEqual('blank_profile.jpg');
+    })
+})
