65d5a54d655502f68f3fd1fb2237b32db1b645cb
Yanis Dib <ynsprive@gmail.com>
2021-11-14 20:52:06 +0100

    Add new components exports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,12 +2,16 @@
 
 export { default as Header } from './Header/Header';
 
+export { default as PictureIdentifier } from './PictureIdentifier/PictureIdentifier';
+
 export { default as Button } from './Button/Button';
 export { default as MenuButton } from './MenuButton/MenuButton';
-export { default as DisplayIdentifiedButton } from './DisplayIdentifiedButton/DisplayIdentifiedButton';
+export { default as DisplayIdentifiedPeopleButton } from './DisplayIdentifiedPeopleButton/DisplayIdentifiedPeopleButton';
 
 export { default as Tag } from './Tag/Tag';
 export { default as Pointer } from './Pointer/Pointer';
 export { default as Bubble } from './Bubble/Bubble';
 
-export { default as PictureIdentifier } from './PictureIdentifier/PictureIdentifier';
+export { default as Slider } from './Slider/Slider';
+export { default as TagsSlider } from './TagsSlider/TagsSlider';
+export { default as SliderNavigationButton } from './SliderNavigationButton/SliderNavigationButton';
