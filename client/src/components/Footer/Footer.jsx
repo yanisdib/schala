7792c3cf8c519fb009e6f51a1d9a23e5087edade
Yanis Dib <ynsprive@gmail.com>
2021-11-19 20:01:37 +0100

    Create Footer.jsx

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className="footer container">
+                <div className="footer-wrapper">
+                    hello world!
+                </div>
+            </div>
+        </footer>
+    )
+}
