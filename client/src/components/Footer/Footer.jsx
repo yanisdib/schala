b4ebff9a969eca291fd0f2cc2dc2ff098fa9c412
Yanis Dib <ynsprive@gmail.com>
2021-11-20 22:58:16 +0100

    Add Footer component and styles

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,13 +1,64 @@
-import React from 'react'
+import './styles.scss';
+
 
 export default function Footer() {
     return (
         <footer>
             <div className="footer container">
                 <div className="footer-wrapper">
-                    hello world!
+                    <div className="footer-content">
+                        <section className="footer-item">
+                            <h4>Support</h4>
+                            <ul>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                            </ul>
+                        </section>
+                        <section className="footer-item">
+                            <h4>Community</h4>
+                            <ul>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                            </ul>
+                        </section>
+                        <section className="footer-item">
+                            <h4>Spotting</h4>
+                            <ul>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                            </ul>
+                        </section>
+                        <section className="footer-item">
+                            <h4>About</h4>
+                            <ul>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                                <li>Lorem ipsum dolores</li>
+                            </ul>
+                        </section>
+                    </div>
+                </div>
+                <div className="footer-copyrights">
+                    <div>
+                        <p>
+                            © 2021 Schala, Inc.
+                            <br />
+                            Confidentialité · Conditions générales · Plan du site · Fonctionnement du site · Infos sur l'entreprise
+                        </p>
+                    </div>
                 </div>
             </div>
         </footer>
-    )
+    );
 }
