1a6fec0f3f5da2734561850080166ecd67e8863b
Yanis Dib <ynsprive@gmail.com>
2021-11-28 22:35:10 +0100

    Update Outfit page

--- client/src/pages/Outfit/Outfit.jsx
+++ client/src/pages/Outfit/Outfit.jsx
@@ -1,43 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Tag, TagsSlider, PictureIdentifier, Button } from '../../components';
-import { MapPin, Heart, MoreHorizontal, Bell, BellOff } from '@styled-icons/feather';
+import {
+    PictureIdentifier,
+    SpotterLastOutfitsGrid,
+    OutfitTags,
+    OutfitItemData,
+    OutfitSpotterBanner,
+    OutfitItemAvailabilities,
+    OutfitSocialFeatures
+} from '../../components';
 
 import './styles.scss';
-import styled from 'styled-components';
-import RoundButton from '../../components/RoundButton/RoundButton';
 
 
-const SmallMapPin = styled(MapPin)`
-    height: 14px;
-    stroke-width: 2px;
-    margin-right: 3px;
-`;
-
-const SmallHeart = styled(Heart)`
-    height: 14px;
-    margin-right: 3px;
-`;
-
-const SmallMoreHorizontal = styled(MoreHorizontal)`
-    height: 14px;
-    margin-right: 3px;
-`;
-
-const SmallBell = styled(Bell)`
-    height: 14px;
-    margin-right: 3px;
-`;
-
-const SmallBellOff = styled(Bell)`
-    height: 14px;
-    margin-right: 3px;
-`;
-
-
-function Outfit({ tags }) {
-
+function Outfit({ outfit }) {
     return (
         <div className="outfit">
             <div className="container outfit-data">
@@ -46,46 +23,16 @@ function Outfit({ tags }) {
                         <PictureIdentifier />
                     </div>
                     <div className="outfit-selected-item-data">
-                        <div className="tags">
-                            <TagsSlider Tags={tagsProps => tags.map((tag, i) => <Tag key={`tag-${i}`} {...tagsProps}>{tag}</Tag>)} />
-                        </div>
-                        <div className="">
-                            <p className="geolocation"><SmallMapPin /> Los Angeles International Airport</p>
-                            <h1 className="title">GUCCI</h1>
-                            <p className="subtitle">Linen pants embroided with flowers</p>
-                            <p className="price">490€</p>
-                            <div className="outfit-social-features">
-                                <Button>Visit the store</Button>
-                                <div className="social-buttons">
-                                    <RoundButton>
-                                        {
-                                            // TODO: Enable/Disable notifications (Bell or BellOff icon)
-                                        }
-                                        <SmallHeart />
-                                    </RoundButton>
-                                    <RoundButton>
-                                        {
-                                            // TODO: Enable/Disable alerts about the current item (Bell or BellOff icon)
-                                        }
-                                        <SmallBell />
-                                    </RoundButton>
-                                    {
-                                        // TODO: Dropdown Button °°° (Add to list, share on social networks, etc)
-                                    }
-                                    <RoundButton>
-                                        <SmallMoreHorizontal />
-                                    </RoundButton>
-                                </div>
-                            </div>
-                        </div>
+                        <OutfitTags />
+                        <OutfitItemData>
+                            <OutfitSocialFeatures />
+                        </OutfitItemData>
+                        <OutfitItemAvailabilities />
                     </div>
                 </div>
-                <div className="outfit-spotter-banner">
-                    <div className="user-picture-thumbnail"><img src="https://cdn-prod.scalefast.com/public/assets/img/resized/squareenix-store-v3/45ad353035234d9d4a1f25d8a9ae9e7f_1920_KR.jpg" /></div>
-                    <div className="user-features">
-                        <RoundButton size={40} borderColor="#005aff"></RoundButton>
-                        <RoundButton size={40} borderColor="#005aff"></RoundButton>
-                    </div>
+                <OutfitSpotterBanner />
+                <div className="spotter-outfits-preview">
+                    <SpotterLastOutfitsGrid />
                 </div>
             </div>
         </div>
@@ -93,19 +40,9 @@ function Outfit({ tags }) {
 }
 
 
-Outfit.defaultProps = {
-    tags: [
-        'Runway',
-        'Luxury',
-        'Haute-couture',
-        "Spring",
-        "Dior",
-        "Gucci",
-        "Football"
-    ]
-}
+Outfit.defaultProps = {}
 
-Outfit.propTypes = { tags: PropTypes.arrayOf(PropTypes.string) }
+Outfit.propTypes = { outfits: PropTypes.arrayOf(PropTypes.object).isRequired }
 
 
 export default Outfit;
