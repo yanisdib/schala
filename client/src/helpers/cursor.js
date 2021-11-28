2c384fd1ce4baf4efb60cd6c8331663a0eb05248
Yanis Dib <ynsprive@gmail.com>
2021-11-28 22:34:12 +0100

    Update cursor.js

--- client/src/helpers/cursor.js
+++ client/src/helpers/cursor.js
@@ -2,9 +2,10 @@ export const getCursorPosition = (event) => {
     const image = event.target.getBoundingClientRect();
     const x = event.clientX - image.left;
     const y = event.clientY - image.top;
-    const cursor = [x, y];
+    const cursorPosition = [x, y];
 
-    return cursor;
+    return cursorPosition;
 }
 
+// Transform a pixel position to a percentage
 export const convertPositionToPercentage = (position, base) => (position / base) * 100;
