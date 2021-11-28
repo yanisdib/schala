fb83df3a97d9f3892b9d409069e501a89fac18b9
Yanis Dib <ynsprive@gmail.com>
2021-11-28 22:27:59 +0100

    Update component exports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -12,6 +12,18 @@ export { default as Tag } from './Tag/Tag';
 export { default as Pointer } from './Pointer/Pointer';
 export { default as Bubble } from './Bubble/Bubble';
 
+export { default as DropdownList } from './DropdownList/DropdownList';
+
 export { default as Slider } from './Slider/Slider';
 export { default as TagsSlider } from './TagsSlider/TagsSlider';
 export { default as SliderNavigationButton } from './SliderNavigationButton/SliderNavigationButton';
+
+export { default as ImagesGrid } from './ImagesGrid/ImagesGrid';
+
+export { default as OutfitTags } from './OutfitTags/OutfitTags';
+export { default as OutfitItemData } from './OutfitItemData/OutfitItemData';
+export { default as OutfitSocialFeatures } from './OutfitSocialFeatures/OutfitSocialFeatures';
+export { default as OutfitItemAvailabilities } from './OutfitItemAvailabilities/OutfitItemAvailabilities';
+export { default as OutfitItemAvailabilitiesList } from './OutfitItemAvailabilitiesList/OutfitItemAvailabilitiesList';
+export { default as OutfitSpotterBanner } from './OutfitSpotterBanner/OutfitSpotterBanner';
+export { default as SpotterLastOutfitsGrid } from './SpotterLastOutfitsGrid/SpotterLastOutfitsGrid';
