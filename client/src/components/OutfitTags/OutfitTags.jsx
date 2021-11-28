ae14ead34ffe09fd2481f67402e0303f745edd35
Yanis Dib <ynsprive@gmail.com>
2021-11-28 22:30:09 +0100

    Add OutfitTags component

---
+++ client/src/components/OutfitTags/OutfitTags.jsx
@@ -0,0 +1,42 @@
+import { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+
+import { Tag, TagsSlider } from '../';
+
+
+function OutfitTags({ tags }) {
+    const tagsList = forwardRef((tagsProps, tagsRef) => {
+        return tags.map((tag, i) => (
+            <Tag
+                {...tagsProps}
+                ref={tagsRef}
+                key={`tag-${i}`}
+            >
+                {tag}
+            </Tag>
+        ));
+    });
+
+    return (
+        <div className="tags">
+            <TagsSlider Tags={tagsList} />
+        </div>
+    );
+}
+
+OutfitTags.defaultProps = {
+    tags: [
+        'Runway',
+        'Luxury',
+        'Haute-couture',
+        "Spring",
+        "Dior",
+        "Gucci",
+        "Football"
+    ]
+}
+
+OutfitTags.propTypes = { tags: PropTypes.arrayOf(PropTypes.string).isRequired }
+
+
+export default OutfitTags;
