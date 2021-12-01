e79bc94e754f75aa4409f8cf9849ea6a9b888843
Yanis Dib <ynsprive@gmail.com>
2021-12-01 18:05:09 +0100

    Add outfits reducer

---
+++ client/src/services/redux/reducers/index.js
@@ -0,0 +1,1 @@
+export { default as outfitsReducer } from './outfits';
---
+++ client/src/services/redux/reducers/outfits.js
@@ -0,0 +1,19 @@
+const initialState = [];
+
+export const outfitsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'outfits/fetchOutfitsRequested':
+            return [...state, action.isLoading];
+        case 'outfits/fetchOutfitsSucceeded':
+            return [...action.outfits, action.isLoading];
+        case 'outfits/addOutfitRequested':
+            return [...state, action.isLoading];
+        case 'outfits/addOutfitSucceeded':
+            return [action.outfits, action.isLoading];
+        case 'outfits/editOutfitsRequested':
+            return [action.outfits, action.isLoading];
+        case 'outfits/editOutfitsSucceeded':
+            return [{ data: action.outfits }, action.isLoading];
+        default: return state;
+    }
+}
