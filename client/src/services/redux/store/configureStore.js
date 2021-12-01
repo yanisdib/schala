2cf742fdc565d7a852e0355baa6675a9d9de9d38
Yanis Dib <ynsprive@gmail.com>
2021-12-01 18:07:43 +0100

    Add store configurations

---
+++ client/src/services/redux/store/configureStore.js
@@ -0,0 +1,20 @@
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import createSagaMiddleware from '@redux-saga/core';
+import { outfitsReducer } from '../reducers';
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const sagaMiddleware = createSagaMiddleware();
+const reducers = combineReducers({ outfits: outfitsReducer });
+const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+
+const configureStore = () => {
+    const store = createStore(reducers, enhancer);
+    sagaMiddleware.run(sagas);
+
+    return store;
+}
+
+
+export default configureStore;
