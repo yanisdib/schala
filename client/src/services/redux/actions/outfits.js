f8fc13916972b00f284f6c494f70db5331149eb7
Yanis Dib <ynsprive@gmail.com>
2021-12-01 15:00:52 +0100

    Add Redux actions for outfits

---
+++ client/src/services/redux/actions/outfits.js
@@ -0,0 +1,44 @@
+export const fetchOutfitsRequest = ({ id = undefined }) => ({ type: 'outfits/fetchOutfitsRequested', isLoading: true });
+
+export const fetchOutfitsSuccess = (outfits) => ({
+    type: 'outfits/fetchOutfitsSucceeded',
+    outfits,
+    isLoading: false
+});
+
+export const addOutfitRequest = (outfits = [{}]) => ({
+    type: 'outfits/addOutfitRequested',
+    outfits,
+    isLoading: true
+});
+
+export const addOutfitSuccess = (outfits) => ({
+    type: 'outfits/addOutfitSucceeded',
+    outfits,
+    isLoading: false
+});
+
+export const updateOutfitRequest = (id, updates = {}) => ({
+    type: 'outfits/editOutfitRequested',
+    id,
+    updates,
+    isLoading: true
+});
+
+export const updateOutfitSuccess = (outfits) => ({
+    types: 'outfits/updateOutfitSucceeded',
+    outfits,
+    isLoading: false
+});
+
+export const deleteOutfitRequest = ({ id = undefined }) => ({
+    type: 'outfits/deleteOufitRequested',
+    id,
+    isLoading: true
+});
+
+export const deleteOutfitSuccess = (outfits) => ({
+    type: 'outfits/deleteOutfitSucceeded',
+    outfits,
+    isLoading: false
+});
