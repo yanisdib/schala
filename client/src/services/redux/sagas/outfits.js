272f648e496cbf9b3bfd36f753eb264b4c727ae0
Yanis Dib <ynsprive@gmail.com>
2021-12-01 18:05:42 +0100

    Add outfits sagas

---
+++ client/src/services/redux/sagas/outfits.js
@@ -0,0 +1,16 @@
+import { fetchOutfitsSuccess } from '../actions/outfits';
+
+
+function* fetchOutfits(action) {
+    try {
+        const outfits = [{}];
+        yield put(fetchOutfitsSuccess(outfits));
+    } catch (error) {
+        yield put({ type: 'outfits/fetchOutfitsFailed', error: error, isLoading: false });
+    }
+}
+
+function* outfitsSaga() { yield takeLatest('outfits/fetchOutfitsRequested', fetchOutfits); }
+
+
+export default outfitsSaga;
